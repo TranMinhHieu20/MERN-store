dfb0ed650c9f0951f9f80f4fff4e530e8188e6aa
tranminhhieu <tranhieu200304@gmail.com>
2025-03-25 23:52:06 +0700

    contructor-header

--- client/src/components/HeaderComponent/HeaderComponent.jsx
+++ client/src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Col } from "antd";
+import { WrapperHeader } from "./style";
 
 const HeaderComponent = () => {
-  return <div>HeaderComponent</div>;
+  return (
+    <div>
+      <WrapperHeader>
+        <Col span={6}>Col</Col>
+        <Col span={12}>Col</Col>
+        <Col span={6}>Col</Col>
+      </WrapperHeader>
+    </div>
+  );
 };
 
 export default HeaderComponent;
