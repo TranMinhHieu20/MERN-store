699af0ea6407524061381b19df9f1632d4f8ee7a
tranminhhieu <tranhieu200304@gmail.com>
2025-03-26 10:51:07 +0700

    Update constructor  header

--- client/src/components/HeaderComponent/HeaderComponent.jsx
+++ client/src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,14 +1,52 @@
 import React from "react";
 import { Col } from "antd";
-import { WrapperHeader } from "./style";
+import {
+  WrapperHeader,
+  WrapperTextHeader,
+  WrapperHeaderAccount,
+  WrapperTextHeaderSmall,
+} from "./style";
+import {
+  UserOutlined,
+  CaretDownOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
+import Search from "antd/es/input/Search";
 
 const HeaderComponent = () => {
   return (
     <div>
-      <WrapperHeader>
-        <Col span={6}>Col</Col>
-        <Col span={12}>Col</Col>
-        <Col span={6}>Col</Col>
+      <WrapperHeader gutter={10}>
+        <Col span={6}>
+          <WrapperTextHeader>LOGO</WrapperTextHeader>
+        </Col>
+        <Col span={12} style={{ gap: 12 }}>
+          <Search
+            // addonBefore="https://"
+            placeholder="input search text"
+            allowClear
+          />
+        </Col>
+        <Col span={6} style={{ display: "flex", gap: 10 }}>
+          <WrapperHeaderAccount>
+            <UserOutlined style={{ fontSize: 22, marginLeft: 10 }} />
+            <div>
+              <WrapperTextHeaderSmall>Đăng nhập/Đăng ký</WrapperTextHeaderSmall>
+              <div>
+                <span style={{ fontSize: 14 }}>Tài Khoản</span>
+                <CaretDownOutlined />
+              </div>
+            </div>
+          </WrapperHeaderAccount>
+          <div
+            style={{ display: "flex", alignItems: "center", marginLeft: 10 }}
+          >
+            <ShoppingCartOutlined style={{ fontSize: 24, color: "#fff" }} />
+            <WrapperTextHeaderSmall style={{ marginTop: 11 }}>
+              Giỏ hàng
+            </WrapperTextHeaderSmall>
+          </div>
+        </Col>
       </WrapperHeader>
     </div>
   );
--- client/src/components/HeaderComponent/style.js
+++ client/src/components/HeaderComponent/style.js
@@ -2,6 +2,29 @@ import styled from "styled-components";
 import { Row } from "antd";
 
 export const WrapperHeader = styled(Row)`
-  padding: 10px 120px;
+  padding: 15px 120px;
   background-color: rgb(26, 148, 255);
+  align-items: center;
+`;
+
+export const WrapperTextHeader = styled.span`
+  font-size: 18px;
+  color: #fff;
+  font-weight: bold;
+  text-align: left;
+  display: flex;
+  justify-content: center;
+`;
+
+export const WrapperHeaderAccount = styled.div`
+  display: flex;
+  align-items: center;
+  color: #fff;
+  gap: 10px;
+  font-size: 12px;
+`;
+
+export const WrapperTextHeaderSmall = styled.span`
+  color: #fff;
+  font-size: 12px;
 `;
