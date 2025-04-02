3960c4c15014f24eb3ed53ee8ec397306df4b317
tranminhhieu <tranhieu200304@gmail.com>
2025-04-02 14:40:55 +0700

    config cors

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config({ path: "./.env" }); // Đảm bảo đúng đường dẫn file .env
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const routes = require("./routes/index.js");
 const URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mern-store.ekxudeb.mongodb.net/?retryWrites=true&w=majority&appName=MERN-store`;
@@ -10,6 +11,7 @@ const URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json()); // ✅ Bắt buộc để đọc dữ liệu JSON từ req.body
 app.use(bodyParser.json());
 
